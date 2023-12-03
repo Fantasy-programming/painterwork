995520b321c1647a6a30d71a30ebd4c0196ef32a
Fantasy <37631123+Fantasy-programming@users.noreply.github.com>
2023-12-03 23:34:21 +0000

    Fix: Vercel bullshit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["localhost", ""] },
+  images: {
+    domains: ["localhost", "https://painterwork.vercel.app"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "painterwork.vercel.app",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
