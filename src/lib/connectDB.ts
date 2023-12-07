988d4d7f7869479278a50a6b51e3cd0ba84e040c
Fantasy <37631123+Fantasy-programming@users.noreply.github.com>
2023-12-07 17:13:57 +0000

    Fix: Potential vercel and mongo fix

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -19,7 +19,13 @@ async function connectDB(): Promise<{
   }
 
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI, {
+      maxPoolSize: 1,
+      minPoolSize: 1,
+      socketTimeoutMS: 10000,
+      serverSelectionTimeoutMS: 10000,
+      maxIdleTimeMS: 10000,
+    });
 
     client = mongoose.connection;
 
